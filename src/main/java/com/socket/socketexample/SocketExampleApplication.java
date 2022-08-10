6f7f0b61db86cfb1490cc2993b488f5da1c3e4fb
qospwmf <qospwmf@gmail.com>
2022-08-10 15:04:09 +0900

    🧹chore : Project Init

---
+++ src/main/java/com/socket/socketexample/SocketExampleApplication.java
@@ -0,0 +1,13 @@
+package com.socket.socketexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocketExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocketExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/socket/socketexample/SocketExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socket.socketexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocketExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
