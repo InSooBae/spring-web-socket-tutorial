da03534d32496a5c3ddf75daec568ee6c6db7be5
qospwmf <qospwmf@gmail.com>
2022-08-22 03:43:03 +0900

    🛠️fix : EmbeddedRedisConfig 순환참조 문제 해결

--- src/main/java/com/socket/socketexample/global/config/EmbeddedRedisConfig.java
+++ src/main/java/com/socket/socketexample/global/config/EmbeddedRedisConfig.java
@@ -1,5 +1,6 @@
 package com.socket.socketexample.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,17 +15,20 @@ import javax.annotation.PreDestroy;
 @Configuration
 public class EmbeddedRedisConfig {
 
-    @Bean
-    @Primary
-    public RedisProperties redisProperties() {
-        return new RedisProperties();
-    }
+//    @Bean
+//    @Primary
+//    public RedisProperties redisProperties() {
+//        return new RedisProperties();
+//    }
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
 
     private RedisServer redisServer;
 
     @PostConstruct
     public void redisServer() {
-        redisServer = new RedisServer(redisProperties().getPort());
+        redisServer = new RedisServer(redisPort);
         redisServer.start();
     }
 
