bf6ff209f5c9cadfaf85621fb61e38e12bd48f92
qospwmf <qospwmf@gmail.com>
2022-08-24 14:55:52 +0900

    ⚡feat : Spring Security 설정 추가 WebSecurityConfig - formLogin() 설정할 경우 인증이 필요한 페이지 접근시 로그인 화면으로 이동되도록 처리됨. - /chat 으로 시작하는 웹페이지 접근 시 USER 권한이 필요 또한 비로그인시 로그인 화면 페이지 이동. - 임시로 인메모리에 계정을 생성하여 이용함 - sockjs 동일한 출처가 컨텐츠 프레임화하도록 구성
    WebSocketConfig
    - 다른 도메인이 사이트에 액세스하도록 허용
    - 웹 소켓 인증 부여

---
+++ src/main/java/com/socket/socketexample/global/config/WebSecurityConfig.java
@@ -0,0 +1,73 @@
+package com.socket.socketexample.global.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.springframework.security.config.Customizer.*;
+
+/**
+ * Web Security 설정
+ */
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain web(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable() // 기본값이 on인 csrf 취약점 보안을 해제한다. on으로 설정해도 되나 설정할경우 웹페이지에서 추가처리가 필요함.
+                .headers(headers -> headers
+                        .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin) // SockJS는 기본적으로 HTML iframe 요소를 통한 전송을 허용하지 않도록 설정되는데 해당 내용을 해제한다.
+                )
+                .authorizeRequests(authorize -> authorize
+                        .antMatchers("/chat/**").hasRole("USER") // chat으로 시작하는 리소스에 대한 접근 권한 설정
+                        .anyRequest().permitAll()   // 나머지 리소스에 대한 접근 설정
+                )
+                .formLogin(withDefaults()); // 권한없이 페이지 접근하면 로그인 페이지로 이동한다.
+
+        return http.build();
+    }
+
+    /**
+     * 테스트를 위해 In-Memory에 계정을 임의로 생성한다.
+     * 서비스에 사용시에는 DB데이터를 이용하도록 수정이 필요하다.
+     */
+    @Bean
+    public UserDetailsService inMemoryUsers() {
+        List<UserDetails> users = new ArrayList<>();
+        users.add(
+                User.builder()
+                        .username("qospwmf")
+                        .password("{noop}1234")
+                        .roles("USER")
+                        .build()
+        );
+
+        users.add(
+                User.builder()
+                        .username("abc")
+                        .password("{noop}1234")
+                        .roles("USER")
+                        .build()
+        );
+
+        users.add(
+                User.builder()
+                        .username("guest")
+                        .password("{noop}1234")
+                        .roles("GUEST")
+                        .build()
+        );
+
+        return new InMemoryUserDetailsManager(users);
+    }
+}
--- src/main/java/com/socket/socketexample/global/config/WebSocketConfig.java
+++ src/main/java/com/socket/socketexample/global/config/WebSocketConfig.java
@@ -3,12 +3,14 @@ package com.socket.socketexample.global.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
+import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.*;
 
 @RequiredArgsConstructor
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+public class WebSocketConfig extends AbstractSecurityWebSocketMessageBrokerConfigurer {
 
 
     @Override
@@ -22,5 +24,20 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/sub");
         config.setApplicationDestinationPrefixes("/pub");
+    }
+
+    // 다른 도메인이 사이트에 액세스하도록 허용 (Spring Security의 보호를 비활성화)
+    @Override
+    protected boolean sameOriginDisabled() {
+        return true;
+    }
+
+    @Override
+    protected void configureInbound(MessageSecurityMetadataSourceRegistry messages) {
+        messages
+                .nullDestMatcher().authenticated()  // 대상이 없는 모든 메시지는 사용자 인증 요구
+                .simpDestMatchers("/chat/**").hasRole("USER")   // /chat/으로 시작하는 모든 메시지는 사용자에게 USER 역할이 있어야함
+                .anyMessage().denyAll();    // 이외 다른 메시지 거부
+
     }
 }
