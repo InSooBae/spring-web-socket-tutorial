1305a4045c8e1dd8d191c6c9caf8e78b78c7b85a
qospwmf <qospwmf@gmail.com>
2022-11-03 13:08:31 +0900

    [Security 적용]🔄update : jwt token으로 채팅 인증 적용 - ChannelTopic, redisListener 일원화

--- src/main/java/com/socket/socketexample/domain/chatting/controller/ChatController.java
+++ src/main/java/com/socket/socketexample/domain/chatting/controller/ChatController.java
@@ -7,6 +7,8 @@ import com.socket.socketexample.domain.chatting.repository.ChatRoomRepository;
 import com.socket.socketexample.domain.pubsub.service.RedisPublisher;
 import com.socket.socketexample.global.security.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
@@ -15,22 +17,28 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class ChatController {
 
-    private final RedisPublisher redisPublisher;
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    private final ChannelTopic channelTopic;
+
     private final ChatRoomRepository chatRoomRepository;
     private final JwtTokenProvider jwtTokenProvider;
 
     /**
      * websocket "/pub/chat/message"로 들어오는 메시징을 처리한다.
      */
+
     @MessageMapping("/chat/message")
     public void message(ChatMessage message, @Header("token") String token) {
         String nickname = jwtTokenProvider.getUserNameFromJwt(token);
-
+        // 로그인 회원 정보로 대화명 설정
+        message.setSender(nickname);
+        // 채팅방 입장시에는 대화명과 메시지를 자동으로 세팅한다.
         if (MessageType.ENTER.equals(message.getType())) {
-            chatRoomRepository.enterChatRoom(message.getRoomId());
+            message.setSender("[알림]");
             message.setMessage(nickname + "님이 입장하셨습니다.");
         }
-        // Websocket에 발행된 메시지를 redis로 발행한다(publish)
-        redisPublisher.publish(chatRoomRepository.getTopic(message.getRoomId()), message);
+        // Websocket에 발행된 메시지를 redis로 발행(publish)
+        redisTemplate.convertAndSend(channelTopic.getTopic(), message);
     }
 }
